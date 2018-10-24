313c1095f5f26c74a6c94dd6b51a7e1e666fe331
Cherkasov Kirill <cherkasov.kirill@gmail.com>
2018-10-24 14:17:18 +0500

    done parsing insiders

--- nparse/main_app/models.py
+++ nparse/main_app/models.py
@@ -10,8 +10,10 @@ class Company(models.Model):
 
 
 class Insider(models.Model):
-    name = models.TextField(max_length=256, verbose_name="Name")
+    name = models.TextField(max_length=256, verbose_name="Name", unique=True)
     company = models.ForeignKey(Company, verbose_name="Company", on_delete=models.CASCADE)
+    relation = models.TextField(max_length=64, verbose_name="Relation", blank=True, null=True)
+    owner_type = models.TextField(max_length=64, verbose_name="Owner type", blank=True, null=True)
 
 
 class HistoryRecord(models.Model):
@@ -27,8 +29,6 @@ class HistoryRecord(models.Model):
 class InsiderRecord(models.Model):
     insider = models.ForeignKey(Insider, verbose_name="Insider", on_delete=models.CASCADE)
     date = models.DateTimeField(verbose_name="Date", blank=True)
-    open = models.FloatField(verbose_name='Open', blank=True, null=True)
-    high = models.FloatField(verbose_name='High', blank=True, null=True)
-    low = models.FloatField(verbose_name='Low', blank=True, null=True)
-    close = models.FloatField(verbose_name='Close/Last', blank=True, null=True)
-    volume = models.FloatField(verbose_name='Volume', blank=True, null=True)
+    shares_traded = models.FloatField(verbose_name="Shares traded", blank=True, null=True)
+    last_price = models.FloatField(verbose_name="Last price", blank=True, null=True)
+    shares_held = models.FloatField(verbose_name="Shares held", blank=True, null=True)
--- nparse/nparse_exec.py
+++ nparse/nparse_exec.py
@@ -16,8 +16,9 @@ class Nparse:
     """main class to run parser and save information to DB"""
     PREF = "https://www.nasdaq.com/symbol/"
     POST_HIST = "/historical/"
-    POST_INST = "/insider-trades/"
+    POST_INST = "/insider-trades"
     HIST_ROWS = 6
+    INS_ROWS = 8
 
     def __init__(self, n=3):
         self.n = n
@@ -48,21 +49,20 @@ class Nparse:
         html_page = requests.get(url, {'User-Agent': headers}).text
         return html_page
 
-    def parse_price(self, task_num, task_list):
+    def parse_price(self, task_num,  task_list):
         '''
         download prices and write them to database
-        :param task_num: number of thread, do not used yet
+        :param task_num: number of thread, not used yet
         :param task_list: list of tickers, parsed in current thread
         :return:
         '''
         HistoryRecord.objects.all().delete()
-        count = 0
         for company in task_list:
             # url = https://www.nasdaq.com/symbol/<ticker>/historical
             url = "".join([self.PREF, company.slug, self.POST_HIST])
             soup = BeautifulSoup(self.get_content(url), 'html.parser')
             tdata = soup.find(id="historicalContainer").find_all('td')
-            for item in self.get_hist_data(tdata):
+            for item in self.get_table_rows(tdata, self.HIST_ROWS):
                 ts = dict({
                     'company': company,
                     'open': item[1].replace(',', ''),
@@ -78,18 +78,56 @@ class Nparse:
                     ts['date'] = timezone.now()
                 rec = HistoryRecord(**ts)
                 rec.save()
-                count += 1
 
-    def get_hist_data(self, tdata):
+    @staticmethod
+    def get_table_rows(tdata, col):
         """
-        return generator of table records for tickets history
-        :param tdata:
+        return generator of where one return value correspond to table row
+        :param tdata: table data
+        :param col: number of columns in table
         :return:
         """
-        for i in range(0, len(tdata), self.HIST_ROWS):
-            yield [''.join(x.text.split()) for x in tdata[i: i+self.HIST_ROWS]]
+        for i in range(0, len(tdata), col):
+            yield [''.join(x.text.split()) for x in tdata[i: i+col]]
 
-    def run(self):
+    def parse_insiders(self, task_num, task_list):
+        '''
+        parse insiders data
+        :param task_num: number of current thread, do not used yet
+        :param task_list: list of tickets to parse
+        :return:
+        '''
+        InsiderRecord.objects.all().delete()
+        for company in task_list:
+            # url = https://www.nasdaq.com/symbol/<ticker>/insider-trades
+            url = "".join([self.PREF, company.slug, self.POST_INST])
+            # print(url)
+            soup = BeautifulSoup(self.get_content(url), 'html.parser')
+            paginator = soup.find_all("a", {"class": "pagerlink"})
+            self.disassemble_insider_page(company, soup)
+            pages = []
+            for page in paginator:
+                try:
+                    pages.append(int(page.text))
+                except ValueError:
+                    continue
+            try:
+                max_page = max(pages)
+                if max_page > 10:
+                    max_page = 10
+                additional_links = []
+            except ValueError:
+                max_page = 0
+            if max_page:
+                for i in range(2, max_page+1):
+                    additional_links.append("".join([url.lower(), '?page={}'.format(i)]))
+                print(additional_links)
+
+    def disassemble_insider_page(self, company, soup):
+        tdata = soup.find("div", {"class": "genTable"}).find_all('td')
+        print(tdata)
+
+    def run_parse_history(self):
         """
         run in n threads pool of jobs
         :return:
@@ -99,6 +137,11 @@ class Nparse:
             task = threading.Thread(target=self.parse_price, args=(task_num, task_list,))
             task.start()
 
+    def run_parse_insiders(self):
+        self.pool = self.create_pool()
+        for task_num, task_list in self.pool.items():
+            task = threading.Thread(target=self.parse_insiders, args=(task_num, task_list,))
+            task.start()
 
     def create_pool(self):
         '''
@@ -120,5 +163,6 @@ class Nparse:
 
 if __name__ == '__main__':
     p = Nparse(n=3)
-    p.init_companies('tickers.txt')
-    p.run()
+    # p.init_companies('tickers.txt')
+    # p.run_parse_history()
+    # p.run_parse_insiders()
